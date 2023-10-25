ea95b478a1255be9f0b68ac58b400288131299c6
Elian Van Cutsem <hello@elian.codes>
2023-10-25 23:42:05 +0200

    chore: add first houston series

--- data/houston.ts
+++ data/houston.ts
@@ -1,5 +1,39 @@
 import { Houston } from "../types";
 
 export const getData = (): Houston[] => [
-	{ name: "Original Houston", command: "houston", icon: "houston" },
+	{
+		id: 1,
+		command: ":houston:",
+		name: "Houston",
+	},
+	{
+		id: 2,
+		command: ":houston_astronaut:",
+		name: "Houstonaut",
+	},
+	{
+		id: 3,
+		command: ":houston_pride:",
+		name: "Pride Houston",
+	},
+	{
+		id: 4,
+		command: ":houston_fuzzy:",
+		name: "Fuzzy Houston",
+	},
+	{
+		id: 5,
+		command: ":houston_princess:",
+		name: "Princess Houston",
+	},
+	{
+		id: 6,
+		command: ":houston_celebrate:",
+		name: "Festive Houston",
+	},
+	{
+		id: 7,
+		command: ":houston_chef:",
+		name: "Chef Houston",
+	},
 ];
